670b8af455329ac3b40be72aa767515407d367d4
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-04-18 15:08:59 +0200

    Change parameter form Location to lat and lng

---
+++ Pharmacies.Data/Interfaces/IPharmacyService.cs
@@ -0,0 +1,17 @@
+﻿using Pharmacies.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacies.Data.Interfaces
+{
+    public interface IPharmacyService
+    {
+        Task<Rootobject> SearchPharmacies(double lat, double lng, int range, string type = "pharmacy");
+        Task<Location> GetSearchedLocation(string input);
+        Rootobject GetObj();
+
+
+    }
+}
