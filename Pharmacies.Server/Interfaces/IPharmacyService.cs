582b9d5a39d25709bd6890fcb980cd2a2fc07a07
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-05-01 11:39:01 +0200

    Moved pharmacy service

---
+++ Pharmacies.Server/Interfaces/IPharmacyService.cs
@@ -0,0 +1,17 @@
+﻿using Pharmacies.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacies.Server.Interfaces
+{
+    public interface IPharmacyService
+    {
+        Task<Rootobject> SearchPharmacies(double lat, double lng, int range, string type = "pharmacy");
+        Task<Location> GetSearchedLocation(string input);
+        Rootobject GetObj();
+
+
+    }
+}
