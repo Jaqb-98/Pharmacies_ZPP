fc17a28c8f3b4db44a2f0c4f44df42182c2ea436
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-05-08 10:49:31 +0200

    Added photos and details getter

--- Pharmacies.Server/Services/PharmacyService.cs
+++ Pharmacies.Server/Services/PharmacyService.cs
@@ -18,13 +18,16 @@ namespace Pharmacies.Server.Services
 
         private readonly IConfiguration _configuration;
 
+        private readonly string _apiKey;
+
         public PharmacyService(IConfiguration configuration)
         {
             _configuration = configuration;
+            _apiKey = _configuration.GetSection("Api").GetSection("ApiKey").Value;
         }
 
 
-        public async Task<Rootobject> SearchPharmacies(double lat,double lng, int range, string type = "pharmacy")
+        public async Task<Rootobject> SearchPharmacies(double lat, double lng, int range, string type = "pharmacy")
         {
             var baseUrl = _configuration.GetSection("Api").GetSection("PlacesApi_nearbysearch");
             var apiKey = _configuration.GetSection("Api").GetSection("ApiKey");
@@ -37,7 +40,7 @@ namespace Pharmacies.Server.Services
             if (response.IsSuccessStatusCode)
             {
                 var responseString = await response.Content.ReadAsStringAsync();
-                return  JsonConvert.DeserializeObject<Rootobject>(responseString);
+                return JsonConvert.DeserializeObject<Rootobject>(responseString);
             }
             else
                 throw new Exception(response.ReasonPhrase);
@@ -65,11 +68,33 @@ namespace Pharmacies.Server.Services
         }
 
 
-    
+        public async Task<Rootobject> GetPlaceDetails(string id)
+        {
+            var baseUrl = _configuration.GetSection("Api").GetSection("details").Value;
+            var parameters = $"place_id={id}&key={_apiKey}&fields=vicinity,name,photo,phone_number,opening_hours";
 
+            var url = $"{baseUrl}{parameters}";
 
+            using var response = await Client.GetAsync(url);
 
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<Rootobject>(responseString);
+            }
+            else
+                throw new Exception(response.ReasonPhrase);
 
+        }
 
+        public string GetPhoto(string photoRef, int width, int height)
+        {
+            var baseUrl = _configuration.GetSection("Api").GetSection("photo").Value;
+            var parameters = $"maxwidth={width}&maxheight={height}&photoreference={photoRef}&key={_apiKey}";
+
+            var url = $"{baseUrl}{parameters}";
+
+            return url;
+        }
     }
 }
