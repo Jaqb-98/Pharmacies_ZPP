d24d4bbeaf3f91640615c593ce7590f32b6c7c75
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-05-23 17:00:05 +0200

    Create 20210522144314_Areas.cs

---
+++ Pharmacies.Server/Migrations/20210522144314_Areas.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pharmacies.Server.Migrations
+{
+    public partial class Areas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.CreateTable(
+                name: "Areas",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AreaName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BoundariesCoords = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Areas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Areas");
+
+        }
+    }
+}
