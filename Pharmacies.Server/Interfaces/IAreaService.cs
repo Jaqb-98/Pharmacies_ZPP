3ee2989c8cf6053e4bdcff752d7aec0456713c9b
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-05-23 16:57:17 +0200

    Areas service

---
+++ Pharmacies.Server/Interfaces/IAreaService.cs
@@ -0,0 +1,15 @@
+﻿using Pharmacies.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacies.Server.Interfaces
+{
+    interface IAreaService
+    {
+        Task AddNewArea(AreaModel area);
+
+        Task<List<AreaModel>> GetAreas();
+    }
+}
---
+++ Pharmacies.Server/Services/AreaService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Pharmacies.Server.Data;
+using Pharmacies.Server.Models;
+using Pharmacies.Server.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pharmacies.Server.Services
+{
+    public class AreaService : IAreaService
+    {
+        private PharmaciesServerContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public AreaService(PharmaciesServerContext context, IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = hostEnvironment;
+        }
+
+        public async Task AddNewArea(AreaModel area)
+        {
+            _context.Areas.Add(area);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<AreaModel>> GetAreas()
+        {
+            var areas = await _context.Areas.ToListAsync();
+            return areas;
+        }
+    }
+}
