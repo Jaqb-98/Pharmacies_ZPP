f8cfa53014267ccee44a91205958edacf7d40166
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-05-23 16:59:36 +0200

    Create AreaModel.cs

---
+++ Pharmacies.Server/Models/AreaModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacies.Server.Models
+{
+    public class AreaModel
+    {
+        public string Id { get; set; }
+        public string AreaName { get; set; }
+        public string Description { get; set; }
+        public string BoundariesCoords { get; set; }
+    }
+}
