84356d905720999aa3ac6da9354211cf703e4ab8
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-05-23 16:57:42 +0200

    Checks if place is in area defined by polygon

---
+++ Pharmacies.Server/wwwroot/js/GoogleMapsJS.js
@@ -0,0 +1,21 @@
+﻿
+
+
+
+IsInArea = (lat, lng, area) => {
+    var location = new google.maps.LatLng(lat, lng);
+
+    var paths = [];
+
+    for (var i = 0; i < area.length; i++) {
+        paths.push(new google.maps.LatLng(area[i].lat, area[i].lng));
+    }
+
+    var polygon = new google.maps.Polygon({
+        paths: paths
+    });
+
+    return google.maps.geometry.poly.containsLocation(location, polygon);
+
+}
+
