a46f8e04463d7d60467397bcccb3e7ec85ccac3b
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-06-05 17:44:09 +0200

    New method to get pharmacies from database

--- Pharmacies.Server/Interfaces/IPharmacyService.cs
+++ Pharmacies.Server/Interfaces/IPharmacyService.cs
@@ -42,5 +42,8 @@ namespace Pharmacies.Server.Interfaces
         string GetPhoto(string photoRef, int width, int height);
 
 
+        Task<ICollection<PharmacyModel>> GetPharmaciesFromDB();
+
+
     }
 }
