c18f74cfde3f96dcdedf4e917ce514189391f447
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-06-13 22:28:06 +0200

    Selenium test base class

---
+++ Pharmacies.Tests/Selenium/SeleniumBase.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacies.Tests
+{
+    public class SeleniumBase :IDisposable
+    {
+        public IWebDriver driver { get; private set; }
+        public IDictionary<String, Object> vars { get; private set; }
+        public IJavaScriptExecutor js { get; private set; }
+        public SeleniumBase()
+        {
+            driver = new ChromeDriver();
+            js = (IJavaScriptExecutor)driver;
+            vars = new Dictionary<String, Object>();
+        }
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+    }
+}
